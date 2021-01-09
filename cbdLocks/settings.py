3f75e21bd7b963b57e0bfcddb5620dea93ffd2a0
JosePorras <joseporras291@gmail.com>
2021-01-09 13:16:04 -0500

    Added postgres database and created the database

--- cbdLocks/settings.py
+++ cbdLocks/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'cbdLocks.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': BASE_DIR / 'cbddb',
+        'USER':'postgres',
+        'PASSWORD':'kikepwns3626',
+        'HOST':'localhost',
+        'PORT': '5432',
     }
 }
 
