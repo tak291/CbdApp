8eab1d429490d3483b938a2d3cc215fcbd385460
JosePorras <joseporras291@gmail.com>
2021-01-09 12:53:13 -0500

    Got the media to work and now we are runnig sqldb3 but goingto change to postgres

--- jobs/models.py
+++ jobs/models.py
@@ -3,3 +3,6 @@ from django.db import models
 class Job(models.Model):
     image = models.ImageField(upload_to='images/')
     summary = models.CharField(max_length=200)
+
+    #nameWho = models.CharField(max_length=200)
+    
