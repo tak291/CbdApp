0952db220b2ad0d9f5c43cf61da3ef145c2e623d
JosePorras <joseporras291@gmail.com>
2021-01-09 21:21:31 -0500

    Added a login app and view and made a home

--- cbdLocks/settings.py
+++ cbdLocks/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'jobs.apps.JobsConfig',
+    'login.apps.LoginConfig',
 ]
 
 MIDDLEWARE = [
--- login/models.py
+++ login/models.py
@@ -1,8 +1,8 @@
-from django.db import models, forms
+from django.db import models
 
 # Create your models here.
 
 class Login(models.Model):
     user = models.CharField(max_length=200)
-    password = models.CharField(widget=forms.PasswordInput, max_length=20)
-     
+    password = models.CharField(max_length=20)
+    
