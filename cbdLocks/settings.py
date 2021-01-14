95b6c2331398a995374cbcecd07ed18edc83731a
JosePorras <joseporras291@gmail.com>
2021-01-13 23:07:19 -0500

    Added a few things tothe databas and played around aeith it.

--- cbdLocks/settings.py
+++ cbdLocks/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'cbddb',
         'USER':'postgres',
-        'PASSWORD':'kikepwns3626',
+        'PASSWORD':'#######',
         'HOST':'localhost',
         'PORT': '5432',
     }
